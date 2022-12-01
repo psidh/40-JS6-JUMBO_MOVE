fb0bc8e2657e861f2e61f047474a24efb461f640
P SIDHARTH <philkhanasidharth14@gmail.com>
2022-12-01 12:32:31 +0530

    Add files via upload

---
+++ JUMBO.js
@@ -0,0 +1,14 @@
+var x = 25;
+var y = 14;
+draw = function() {
+
+
+background(232, 0, 0);
+image(getImage("avatars/robot_male_3"), y, x, 297, 200);
+  x += 1/4;
+  y += 1/5;
+};
+// Draw the image at its default size
+
+// Draw a smaller image
+
